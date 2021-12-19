fca8840329ac80a4ac513172ba1607efce16184c
Aibek Ramazanov <aibekramazanov@Aibeks-MBP.lan>
2021-12-19 13:59:24 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+    { title: "War and Peace", author: "Dostoevsky" },
+    { title: "harry potter", author: "Joanna Rollings" }
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Booklist() {
+    return (
+        <section>
+            {books.map((book) => {
+                return <Book {...book} />;
+            })}
+        </section>
+    );
+}
+
+const Book = (props) => {
+    console.log(props);
+    const { title, author } = props;
+    const clickHandler = () => {
+        alert("hello world");
+    };
+
+    const complexExample = (author) => {
+        console.log(author);
+    };
+
+    return (
+        <article>
+            <h4 onClick={() => console.log(title)}>{title}</h4>
+            <h4>{author}</h4>
+            <button type="button" onClick={clickHandler}>
+                reference example
+            </button>
+            <button type="button" onClick={() => complexExample(author)}>
+                more complex example
+            </button>
+        </article>
+    );
+};
+
+ReactDom.render(<Booklist />, document.getElementById("root"));
